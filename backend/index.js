c8599c0ce091397978c19193607f60306bb53763
Saúl Camacho <electrohead1200@gmail.com>
2024-06-04 12:10:24 -0600

    Frontend config + tailwind, react, vite

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { connectToDB } from "../backend/db.js";
 import booksRoute from "./routes/booksRoute.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 const port = 4000;
@@ -10,6 +11,15 @@ dotenv.config(); // Leer .env
 
 // Parsing Request Body
 app.use(express.json()); // Leer Json
+
+//  Middleware for handling CORS Policy
+app.use(
+  cors({
+    origin: "http://localhost:4000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 connectToDB();
 
